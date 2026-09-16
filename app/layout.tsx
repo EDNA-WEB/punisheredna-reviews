9365d59de3895f5fd8f7bf8cc937b8b62e7348e2
PunisherEDNA <punisheredna@punisheredna.com>
2026-09-16 11:53:20 +0200

    drobna uprava komentara

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ const body = Inter({
   weight: ['400', '500', '600', '700']
 });
 
-// Vynúti dynamické (server-side) vykresľovanie pri každej požiadavke.
+// Vynúti dynamické (server-side) vykresľovanie pri každej HTTP požiadavke.
 export const dynamic = 'force-dynamic';
 
 const siteUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
